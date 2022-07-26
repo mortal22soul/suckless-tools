7021059c6a02ce059fc2f56ad43b70ae297cd50e
Aryan <99786026+Mortal22Soul@users.noreply.github.com>
2022-07-26 15:44:54 +0530

    Update catppuccin.h

--- chadwm/themes/catppuccin.h
+++ chadwm/themes/catppuccin.h
@@ -1,4 +1,5 @@
 static const char black[]       = "#1E1D2D";
+static const char white[]       = "#f8f8f2";
 static const char gray2[]       = "#282737"; // unfocused window border
 static const char gray3[]       = "#585767";
 static const char gray4[]       = "#282737";
